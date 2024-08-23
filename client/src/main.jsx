1695d58157a4045148cf42ff494748b21f61cc39
Sahil <123401404+Sahil-911@users.noreply.github.com>
2024-08-23 22:58:57 +0530

    clear unwanted code

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import CssBaseline from "@mui/material/CssBaseline";
 import App from "./App.jsx";
 import "./main.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <CssBaseline /> */}
     <App />
   </StrictMode>
 );
